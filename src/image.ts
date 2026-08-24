ddeda0eec15375e14eca05117736370553497abf
Aleksei Rudenko <rudenkoaleksey@icloud.com>
2026-08-24 09:50:47 +0300

    init commit and some initial utilities

--- src/image.ts
+++ src/image.ts
@@ -1,20 +1,25 @@
 import sharp from "sharp";
 
-export interface GrayImage {
+interface GrayImage {
   width: number;
   height: number;
   pixels: Uint8Array;
 }
 
-export async function loadGrayImg(
-    filename: string
-): Promise<GrayImage> {
-    const {data, info} = await sharp(filename).grayscale().raw().toBuffer({resolveWithObject: true})
+interface GridPeak {
+  start: number | null;
+  end: number | null;
+  center: number | null;
+}
 
-    if (info.channels !== 1) {
-    throw new Error(
-      `Expected 1 channel, got ${info.channels}`,
-    );
+export async function loadGrayImg(filename: string): Promise<GrayImage> {
+  const { data, info } = await sharp(filename)
+    .grayscale()
+    .raw()
+    .toBuffer({ resolveWithObject: true });
+
+  if (info.channels !== 1) {
+    throw new Error(`Expected 1 channel, got ${info.channels}`);
   }
 
   return {
@@ -24,18 +29,8 @@ export async function loadGrayImg(
   };
 }
 
-
-export function getGrayImagePixel(
-  image: GrayImage,
-  x: number,
-  y: number,
-): number {
-  if (
-    x < 0 ||
-    y < 0 ||
-    x >= image.width ||
-    y >= image.height
-  ) {
+export function getGrayImagePixel(image: GrayImage, x: number, y: number): number | undefined {
+  if (x < 0 || y < 0 || x >= image.width || y >= image.height) {
     throw new RangeError(`Pixel outside image: ${x}, ${y}`);
   }
 
@@ -46,13 +41,199 @@ export function getDarkness(pixel: number): number {
   return 255 - pixel;
 }
 
-
 export function getColumnDarkness(image: GrayImage, columnNumber: number): number {
-    let summ = 0;
-    let currentRow = 0
-    // итерируемся по стркоам, то есть каждый тик это currentRow + 
-    for(let currentRow = 0; currentRow <= ) {
+  if (columnNumber < 0 || columnNumber >= image.width) {
+    throw new RangeError(`Column number ${columnNumber} is outside the image`);
+  }
 
+  let sum = 0;
+  for (let currentRow = 0; currentRow < image.height; currentRow++) {
+    const index = currentRow * image.width + columnNumber;
+    const pixel = image.pixels[index];
+
+    if (typeof pixel === "undefined") {
+      throw new RangeError(`Wrong pixel value at the position {${index}}`);
     }
 
+    sum = sum + getDarkness(pixel);
+  }
+
+  return sum;
+}
+
+export function getRowDarkness(image: GrayImage, rowNumber: number): number {
+  if (rowNumber < 0 || rowNumber >= image.height) {
+    throw new RangeError(`Row number ${rowNumber} is outside the image`);
+  }
+
+  let sum = 0;
+
+  for (let currentCol = 0; currentCol < image.width; currentCol++) {
+    const index = rowNumber * image.width + currentCol;
+    const pixel = image.pixels[index];
+
+    if (typeof pixel === "undefined") {
+      throw new RangeError(`Wrong pixel value at the position {${index}}`);
+    }
+
+    sum = sum + getDarkness(pixel);
+  }
+
+  return sum;
+}
+
+// TODO: мемоизировать результат!
+export function getXDarknessProjection(image: GrayImage): number[] {
+  const result: number[] = [];
+  for (let currentCol = 0; currentCol < image.width; currentCol++) {
+    const columnDarkness = getColumnDarkness(image, currentCol);
+    result.push(columnDarkness);
+    // console.log(`[${currentCol}]: ${columnDarkness}`);
+  }
+
+  return result;
+}
+
+// TODO: мемоизировать результат!
+export function getYDarknessProjection(image: GrayImage): number[] {
+  const result: number[] = [];
+  for (let currentRow = 0; currentRow < image.height; currentRow++) {
+    const rowDarkness = getRowDarkness(image, currentRow);
+    result.push(rowDarkness);
+  }
+
+  return result;
+}
+
+export function getAverageDarkness(image: GrayImage): number {
+  const projection = getXDarknessProjection(image);
+  let sum = 0;
+  for (let i = 0; i < projection.length; i++) {
+    const columnProjection = projection[i];
+
+    if (typeof columnProjection !== "number") {
+      throw new RangeError(`Wrong column projection value ${columnProjection}`);
+    }
+
+    sum += columnProjection;
+  }
+
+  return sum / projection.length;
+}
+
+export function getXPeaks(image: GrayImage): GridPeak[] {
+  const projection = getXDarknessProjection(image);
+  const average = getAverageDarkness(image);
+
+  const result = [];
+
+  let peakStart = null;
+  let peakEnd = null;
+  let projectionIndex = 0;
+
+  while (projectionIndex < projection.length) {
+    const currentDarkness = projection[projectionIndex];
+
+    if (currentDarkness === undefined) {
+      throw new RangeError(`Wrong projection darkness at the index ${projectionIndex}`);
+    }
+
+    if (peakStart === null && currentDarkness > average) {
+      peakStart = projectionIndex;
+    }
+
+    if (peakStart !== null && currentDarkness <= average) {
+      peakEnd = projectionIndex - 1;
+      result.push({
+        start: peakStart,
+        end: peakEnd,
+        center: peakStart + Math.floor((peakEnd - peakStart) / 2),
+      });
+      peakEnd = null;
+      peakStart = null;
+    }
+
+    projectionIndex++;
+  }
+
+  if (peakStart != null && peakEnd == null) {
+    peakEnd = projection.length - 1;
+    result.push({
+      start: peakStart,
+      end: peakEnd,
+      center: peakStart + Math.floor((peakEnd - peakStart) / 2),
+    });
+  }
+
+  return result;
+}
+
+export function getYPeaks(image: GrayImage): GridPeak[] {
+  const projection = getYDarknessProjection(image);
+  const average = getAverageDarkness(image);
+
+  const result = [];
+
+  let peakStart = null;
+  let peakEnd = null;
+  let projectionIndex = 0;
+
+  while (projectionIndex < projection.length) {
+    const currentDarkness = projection[projectionIndex];
+
+    if (currentDarkness === undefined) {
+      throw new RangeError(`Wrong projection darkness at the index ${projectionIndex}`);
+    }
+
+    if (peakStart === null && currentDarkness > average) {
+      peakStart = projectionIndex;
+    }
+
+    if (peakStart !== null && currentDarkness <= average) {
+      peakEnd = projectionIndex - 1;
+      result.push({
+        start: peakStart,
+        end: peakEnd,
+        center: peakStart + Math.floor((peakEnd - peakStart) / 2),
+      });
+      peakEnd = null;
+      peakStart = null;
+    }
+
+    projectionIndex++;
+  }
+
+  if (peakStart != null && peakEnd == null) {
+    peakEnd = projection.length - 1;
+    result.push({
+      start: peakStart,
+      end: peakEnd,
+      center: peakStart + Math.floor((peakEnd - peakStart) / 2),
+    });
+  }
+
+  return result;
+}
+
+export function getGridStep(peaks: GridPeak[]): number {
+  const result: number[] = [];
+
+  for (let i = 1; i < peaks.length; i++) {
+    const prev = peaks[i - 1];
+    const current = peaks[i];
+
+    if (typeof current?.center !== "number" || typeof prev?.center !== "number") {
+      throw new RangeError(`Incorrect peak center values`);
+    }
+    result.push(current.center - prev.center);
+  }
+
+  result.sort((a, b) => a - b);
+
+  const center = Math.floor(result.length / 2);
+
+  if (typeof result[center] !== "number") {
+    throw new RangeError(`Incorret grid step calculation result`);
+  }
+  return result[center];
 }
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,41 @@
 import path from "node:path";
 
-
-import { getDarkness, getGrayImagePixel, loadGrayImg } from "./image.ts";
+import {
+  getAverageDarkness,
+  getGrayImagePixel,
+  getGridStep,
+  getXPeaks,
+  getYPeaks,
+  loadGrayImg,
+} from "./image.ts";
 
 async function main() {
-    const input = process.argv[2];
+  const input = process.argv[2];
 
-    if (!input) {
-        console.error("Usage: goban2sgf <image>");
-        process.exitCode = 1;
-        return;
-    }
+  if (!input) {
+    console.error("Usage: goban2sgf <image>");
+    process.exitCode = 1;
+    return;
+  }
 
-    const filename = path.resolve(input);
+  const filename = path.resolve(input);
 
   console.log(`Loading ${filename}`);
 
   const image = await loadGrayImg(filename);
 
-  console.log(
-    `Image: ${image.width}x${image.height}`,
-  );
+  console.log(`Image: ${image.width}x${image.height}`);
 
   const centerX = Math.floor(image.width / 2);
   const centerY = Math.floor(image.height / 2);
-  const targetPixel = getGrayImagePixel(
-      image,
-      centerX,
-      centerY,
-    );
+  const targetPixel = getGrayImagePixel(image, centerX, centerY);
 
-console.log(
-    `Center pixel: ${targetPixel}`,
-    );
-    console.log('Darkness:', getDarkness(targetPixel));
+  if (targetPixel !== undefined) {
+    console.log(`Center pixel: ${targetPixel}`);
+    console.log("Average darkness: ", getAverageDarkness(image));
+    console.log("X Peaks:  ", getXPeaks(image), "; Step is ", getGridStep(getXPeaks(image)));
+    console.log("Y Peaks:  ", getYPeaks(image), "; Step is ", getGridStep(getYPeaks(image)));
+  }
 }
 
 main().catch((error) => {
